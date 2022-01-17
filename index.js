73e937db1722ae90c3c1718c28207f59115346aa
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-01-17 15:32:58 -0700

    MVP Task 4/5 Done_4/4 Passed

--- index.js
+++ index.js
@@ -110,10 +110,7 @@ function finalScore(inningCB,inningNum){
     homeScore = homeScore + inningCB();
     awayScore = awayScore + inningCB();
   }
-  return {
-    Home: homeScore,
-    Away: awayScore
-  }
+  return {Home: homeScore,Away: awayScore};
 }
 // Ex Console.log_Output:{Home:Random Score, Away:Random Score}
 // console.log(finalScore(inning, 9))
@@ -123,10 +120,14 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+//Answer_Passed
+function getInningScore(inningCB) {
+  let homeScore = inningCB();
+  let awayScore = inningCB();
+  return {Home:homeScore, Away:awayScore};
 }
-
+// Ex Console.log_Output:{Home:Random Score, Away:Random Score}
+// console.log(getInningScore(inning));
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
